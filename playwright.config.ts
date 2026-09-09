1415bd051fda706e8409430c08dbd4f66032c04d
dhavalb-glitch <dhaval.b@york.ie>
2026-09-09 13:47:36 +0530

    Fix Playwright headless mode in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: 'https://practicetestautomation.com',
-    headless: false,
+    headless: !!process.env.CI,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
